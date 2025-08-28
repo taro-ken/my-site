90a0a57dc30697e998abcca5bca44c586aea01a6
kentaro kimoto <exiler2411k@gmail.com>
2025-08-28 10:32:21 +0900

    ほぼ完成

--- src/data/works.js
+++ src/data/works.js
@@ -1,9 +1,9 @@
 export const worksData = [
     {
         id: 'work-muscle',
-        title: 'Focusy-タスク完了でロック解放-',
+        title: 'Focusy<br>-タスク完了でロック解放-',
         description: '集中力を取り戻し、やるべきことをやり切ろう',
-        techs: ['Flutter', 'Dart', 'iOS', 'Android'],
+        techs: ['Swift', 'Firebase'],
         images: ['/images/works/focusy_1.png', '/images/works/focusy_2.png'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/jp/app/focusy-%E3%82%BF%E3%82%B9%E3%82%AF%E5%AE%8C%E4%BA%86%E3%81%A7%E3%83%AD%E3%83%83%E3%82%AF%E8%A7%A3%E6%94%BE/id6747918924' },
@@ -12,9 +12,9 @@ export const worksData = [
     },
     {
         id: 'work-calorie',
-        title: 'ボディビルノート - 究極の筋トレ記録管理',
+        title: 'ボディビルノート<br>- 究極の筋トレ記録管理 - ',
         description: '種目・セット・レポート・PFC・体重を一括管理',
-        techs: ['Flutter', 'Dart', 'iOS'],
+        techs: ['Swift', 'Firebase'],
         images: ['/images/works/body_1.png', '/images/works/body_2.png'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/app/id6749787325' },
@@ -25,7 +25,7 @@ export const worksData = [
         id: 'work-poke-typing-cli',
         title: 'KindPick',
         description: '読むだけで終わらせない「自分だけの記録を」',
-        techs: ['Nim', 'cli'],
+        techs: ['Swift', 'Firebase', 'AWS', 'node.js'],
         images: ['/images/works/kind_1.png', '/images/works/kind_2.png'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/jp/app/kindpick/id6748674016' },
@@ -36,8 +36,8 @@ export const worksData = [
         id: 'work-zerolines',
         title: 'Minimal Journal',
         description: "手書きで自分だけのジャーナルを",
-        techs: ['TypeScript', 'npm', 'CSS'],
-        images: ['/images/havetodo.jpg', '/images/kindley.jpg'],
+        techs: ['Swift'],
+        images: ['/images/works/journal_1.png', '/images/works/journal_2.png'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/jp/app/minimal-journal/id6749220922' },
         ],
@@ -45,10 +45,10 @@ export const worksData = [
     },
     {
         id: 'work-flat-svg',
-        title: 'MyTube -自分だけのプレイリスト作成-',
+        title: 'MyTube<br>-自分だけのプレイリスト作成-',
         description: 'お気に入りの動画を手元に！',
-        techs: ['SVG', 'Gatsby.js', 'Tailwind.css'],
-        images: ['/images/havetodo.jpg', '/images/kindley.jpg'],
+        techs: ['Swift', 'Youtube Data API'],
+        images: ['/images/works/mytube.jpg'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/jp/app/mytube-%E8%87%AA%E5%88%86%E3%81%A0%E3%81%91%E3%81%AE%E3%83%97%E3%83%AC%E3%82%A4%E3%83%AA%E3%82%B9%E3%83%88%E4%BD%9C%E6%88%90/id6447217754' },
         ],
@@ -56,13 +56,24 @@ export const worksData = [
     },
     {
         id: 'work-claby',
-        title: 'LEFT DAYS -あと何日？-',
+        title: 'LEFT DAYS',
         description: 'Googleカレンダー同期可能',
-        techs: ['Flutter', 'Dart', 'iOS App'],
-        images: ['/images/havetodo.jpg', '/images/kindley.jpg'],
+        techs: ['Swift', 'Google Calendar API'],
+        images: ['/images/works/leftdays.jpg'],
         stores: [
             { label: 'App Store', href: 'https://apps.apple.com/jp/app/left-days-%E3%81%82%E3%81%A8%E4%BD%95%E6%97%A5/id6504957603' },
         ],
         actions: [],
+    },
+    {
+        id: 'work-claby',
+        title: 'dumi jewelry',
+        description: 'jewelrybrand website',
+        techs: ['BASE', 'HTML/CSS', 'JavaScript'],
+        images: ['/images/works/dumi_1.png', '/images/works/dumi_2.png'],
+        stores: [],
+        actions: [
+            { label: 'View More', href: 'https://dumi.official.ec/', target: '_blank' }
+        ],
     },
 ]; 
