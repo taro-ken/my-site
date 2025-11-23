10086b7804e5d1eb82105228d18f5c410610ebe0
kentaro kimoto <exiler2411k@gmail.com>
2025-11-23 18:02:02 +0900

    サーチコンソール対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel()
+  output: 'static'
 });
