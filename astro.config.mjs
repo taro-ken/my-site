fbc55e5b8e45cde6bae3e37c5adc5fa166400879
kentaro kimoto <exiler2411k@gmail.com>
2025-12-20 18:14:47 +0900

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
